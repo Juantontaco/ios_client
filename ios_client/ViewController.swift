031a2bc5e4f0c495edfd8a930e87a1b5a69aef63
mdignan <maxdignan@gmail.com>
2018-05-18 16:29:39 -0400

    init

---
+++ ios_client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_client
+//
+//  Created by Max Dignan on 5/18/18.
+//  Copyright © 2018 RideZoot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
